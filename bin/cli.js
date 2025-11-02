82a623d52f81f2e7d35ee81cc692cef750178ade
Your Name <your.email@example.com>
2025-11-03 09:43:35 +1100

    chore: create CLI directory structure

---
+++ bin/cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+console.log('Blog-CC CLI - Coming soon');
+process.exit(0);
---
+++ lib/cli/index.ts
@@ -0,0 +1,5 @@
+export * from './utils/index.js';
+export * from './content/index.js';
+export * from './validation/index.js';
+export * from './deployment/index.js';
+export * from './site/index.js';
